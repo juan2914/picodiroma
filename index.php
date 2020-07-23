2b50b06610876ee66ad023c40f130c491d5a46bf
samuel <samuel270899@gmail.com>
2020-07-23 10:21:45 -0500

    Modificacio index

--- index.php
+++ index.php
@@ -1,24 +1,2 @@
-<<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-	<<table>
-		<caption>table title and/or explanatory text</caption>
-		<thead>
-			<tr>
-				<th>header</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>data</td>
-			</tr>
-		</tbody>
-	</table>
-</body>
-</html>m
+<a href="#">carrito</a>
+<<a href="#" title="">productos</a>
