b32d11cd3086191c6fc2d7fda55e6c51dd8f83f7
abraham <arabrahson@gmail.com>
2020-07-23 13:49:42 -0500

    cambios

--- index.php
+++ index.php
@@ -1,44 +1,58 @@
-<!DOCTYPE html>
+<!DOCTYPE html>		
 <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <html>
 <head>
-<link  rel="icon"   href="picologo.png" type="style/img/image/png" />
+<link  rel="icon"   href="style/img/picologo.png" type="style/img/image/png" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <title>Pico Di Roma</title>
   <header class="header">
   	<LINK REL=StyleSheet HREF="style/style.css" TYPE="text/css" MEDIA=screen>
   	<!-- Aquí empieza la cabecera de la web -->
-    <div class="header-info">Puedes ser un artista que quiere presentarse a sí mismo y a su trabajo o un negocio con un objetivo que describir 
+    <div class="header-info"><img src="style/img/login.png"> Puedes ser un artista que quiere presentarse a sí mismo y a su trabajo o un negocio con un objetivo que describir 
     <span class="hidden-xs hidden-sm"></span>
     </div>
+
+   
     <!-- Aquí empieza el menu -->
     <div class="menu">
 <nav>
 	
 	<a href="#">Inicio</a>
 	<a href="#">Quienes somos</a>
-	<a href="#">Blog</a>
-	<a href="#">Portefolio</a>
-	<a href="#">Contact</a>
+	<a href="#">Tienda</a>
+	<a href="#">Contacto</a>
+	<a href="#">Cuenta</a>
+	<a href="#"><img src="style/img/comprapico.png"></a>
 	<div class="animation start-home"></div>
 </nav>
 </head>
 
 
 <body>
+	<div class="banner">
+		<img src="style/img/baner.png">
+	</div>
+	 <!-- Aquí empieza la venta -->
+	<table width="50%"border="2" align="center">
+		<tr><td><img src="style/img/picologo.png"></td><td><img src="style/img/picologo.png"></td><td><img src="style/img/picologo.png"></td></tr>
+		<tr><td><img src="style/img/picologo.png"></td><td><img src="style/img/picologo.png"></td><td><img src="style/img/picologo.png"></td></tr>
+		
+
+	</table>
 </body>
 
  <!-- Aquí empieza el pie de pagina -->
-<footer>
+<footer align="center">
 	<table width="70%" border="0" cellspacing="0" cellpadding="0">
 <tr>
 <td rowspan="3"><p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6778420968835!2d-99.17260588453064!3d19.42632064591411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff4a2d6d054f%3A0xbacdc903fbe709be!2sINT.%20702%2C%20Av.%20Paseo%20de%20la%20Reforma%20393%2C%20Ju%C3%A1rez%2C%2006500%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1595439494381!5m2!1ses-419!2smx" width="800" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p></td>
-<td><p><a title="facebook" href="http://www.facebook.com"><img src="style/img/facepico.png"></p></td><td><img src="style/img/whatspico.png"></td><td><a title="instagram" href="http://www.instagram.com"><img src="style/img/instapico.png"></td><td><a title="youtube" href="http://www.youtube.com"><img src="style/img/youpico.png"></td><td><img src="style/img/gmailpico.png"></td><td>España</td>
+<td><p><a title="facebook" href="https://www.facebook.com/PicoDiRoma/ "><img src="style/img/facepico.png"></p></td><td><img src="style/img/whatspico.png"></td><td><a title="instagram" href="https://www.instagram.com/galeria_pico_di_roma/"><img src="style/img/instapico.png"></td><td><a title="youtube" href="http://www.youtube.com"><img src="style/img/youpico.png"></td><td><img src="style/img/gmailpico.png"></td>
 </tr>
 <tr>
-<td colspan="7"><p>Francia</p></td>
+<td colspan="6"><p>Francia</p></td>
 </tr>
 <tr>
-<td colspan="7"><p>Reino Unido</p></td>
+<td colspan="6"><p>Reino Unido</p></td>
 </tr>
 <tr><td colspan="2">© Pico Di Roma 2020
 Creado Avatrade.</td></tr>
